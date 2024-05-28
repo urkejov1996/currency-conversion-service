c05a7a87621d5b1ff9f41b87185906ebed7877f5
urkejov1996 <urkejov1996@gmail.com>
2024-05-28 20:08:31 +0200

    added rest template call to exchange service and exposed endpoint for currency conversion

---
+++ src/main/java/com/urkejov/currency_conversion_service/entity/CurrencyConversion.java
@@ -0,0 +1,34 @@
+package com.urkejov.currency_conversion_service.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class CurrencyConversion {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+
+    public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, BigDecimal totalCalculatedAmount, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.quantity = quantity;
+        this.conversionMultiple = conversionMultiple;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.environment = environment;
+    }
+}
